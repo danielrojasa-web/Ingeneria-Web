2c3d740f81944fd8aa10ef58a754f1fe2f4082f0
danielrojas-web <daniel.rojasa@ecci.edu.co>
2026-08-08 22:35:24 -0500

    dato agosto

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+
+  // Seleccionamos el botón por su ID
+  const boton = document.getElementById('btnGris');
+
+  // Escuchamos el evento click
+  boton.addEventListener('click', () => {
+    console.log('El evento click funciona correctamente');
+    // Aquí puedes meter la lógica que necesites
+  });
